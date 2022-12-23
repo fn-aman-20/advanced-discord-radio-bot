0649637c74e33db957a2a83e0f62cd9aed56e83b
Aman <99836388+fn-aman-20@users.noreply.github.com>
2022-12-23 16:55:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,40 +3,54 @@ host.get('/', (req, res) => res.send('Status: online'));
 host.listen(2022);
 
 const {
-  Client, ActivityType,
-  GatewayIntentBits: {
-    GuildVoiceStates
-  },
-  ChannelType: {
-    GuildStageVoice
-  }
+  Client,
+  ChannelType,
+  GatewayIntentBits
 } = require('discord.js'),
 {
   joinVoiceChannel,
   createAudioPlayer,
   createAudioResource
 } = require('@discordjs/voice'),
-{ exec } = require('child_process'),
-main = require('./config');
+config = require('./util/config'),
+{ EventEmitter } = require('events');
 
 const client = new Client({
   intents: [
-    GuildVoiceStates
-  ]
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ],
+  presence: config.status
 }),
+radio = new EventEmitter(),
 player = createAudioPlayer({
   behaviors: {
-    noSubscriber: 'pause'
+    noSubscriber: 'play'
   }
 }),
-join = (vc) => joinVoiceChannel({
-  channelId: vc.id,
-  guildId: vc.guild.id,
-  adapterCreator: vc.guild.voiceAdapterCreator
-}),
-setTopic = async (channel, topic) => {
+join = (channel) => {
+  let connection = joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator
+  });
+  connection.subscribe(player);
+  connection.on('stateChange', (previous, current) => {
+    if (current.status === 'disconnected') {
+      connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator
+      });
+      connection.subscribe(player);
+    }
+  });
+  return connection;
+},
+res = (...args) => createAudioResource(...args),
+setTopic = (channel, topic) => {
   if (channel.stageInstance?.topic === topic) return;
-  await channel.guild.stageInstances.fetch(channel.id)
+  channel.guild.stageInstances.fetch(channel.id)
   .then(i => {
     if (i.topic === topic) return;
     i.edit({
@@ -52,79 +66,24 @@ setTopic = async (channel, topic) => {
   });
 };
 
-let sources = main.sources, resource,
-length = (24 * 3600)/sources.length,
-tryPlaying = (res) => {
-  try {
-    if (player._state.status === 'playing') {
-      player.stop();
-      player.play(res);
-    } else {
-      player.play(res);
-    }
-  } catch { exec('kill 1') }
-};
-
-client.on('ready', async () => {
-  const channel = await client.channels.fetch(main.channel);
-  setInterval(() => {
-    const d = new Date(),
-    time = (d.getUTCHours() * 3600) + (d.getUTCMinutes() * 60) + d.getUTCSeconds(),
-    position = Math.floor(time/length);
-    if (channel.stageInstance) {
-      if (sources[position].topic === channel.stageInstance.topic) return;
-      resource = createAudioResource(sources[position].url, {
-        metadata: sources[position]
-      });
-      tryPlaying(resource);
-      instance = setTopic(channel, resource.metadata.topic);
-    } else {
-      if (resource) {
-        if (resource.metadata.topic === sources[position].topic) return;
-        resource = createAudioResource(sources[position].url, {
-          metadata: sources[position]
-        });
-        tryPlaying(resource);
-      } else {
-        resource = createAudioResource(sources[position].url, {
-          metadata: sources[position]
-        });
-        tryPlaying(resource);
-        if (channel.type === GuildStageVoice) setTopic(channel, resource.metadata.topic);
-      }
-    }
-  }, 15 * 1000);
-
-  player.on('error', () => tryPlaying(resource));
-  player.on('stateChange', (previous, current) => {
-    console.log(`[audio] :: ${previous.status} => ${current.status}`);
-    if (previous.status !== 'idle' && current.status === 'buffering' || current.status === 'idle') tryPlaying(resource);
-  });
-  
-  const connection = join(channel);
-  connection.subscribe(player);
-  
-  connection.on('ready', async () => {
-    if (channel.type !== GuildStageVoice) return;
-    await channel.guild.members.me.voice.setSuppressed(false);
-  });
-
-  connection.on('stateChange', (previous, current) => {
-    console.log(`[voice] :: ${previous.status} => ${current.status}`);
-    if (current.status === 'disconnected') connection = join(channel);
-  });
-
-  let presence = main.status.activity,
-  activity = {
-    activities: [{
-      name: presence.name.toString() || '24/7',
-      type: typeof presence.type === 'number' ? presence.type : ActivityType.Streaming
-    }]
-  };
-  if (presence.type === ActivityType.Streaming) {
-    activity.activities[0].url = presence.url || 'https://twitch.tv/nocopyrightsounds';
-  } else activity.status = typeof presence.status === 'string' ? presence.status : 'idle';
-  client.user.setPresence(activity);
+player.on('error', () => radio.emit('play'));
+player.on('stateChange', (previous, current) => {
+  if (previous.status === 'playing' && current.status !== 'playing') radio.emit('play', config.sources);
 });
 
-main.onboot(client, process.env.token);
+client.once('ready', async () => {
+  const channel = await client.channels.fetch(config.channel);
+  join(channel);
+  radio.on('play', async (sources) => {
+    const source = sources[Math.floor(Math.random() * sources.length)];
+    if (channel.type === ChannelType.GuildStageVoice) {
+      setTopic(channel, source.topic);
+      await channel.guild.members.me.voice.setSuppressed(false);
+    }
+    player.play(res(source.url));
+    setTimeout(() => radio.emit('play', config.sources), config.interval);
+  });
+  radio.emit('play', config.sources);
+});
+
+config.onboot(client, config.auth);
