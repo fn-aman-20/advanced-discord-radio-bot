5eb45d22a67e0b92170aa0c16f4d1fce0fffd587
Aman <99836388+fn-aman-20@users.noreply.github.com>
2022-12-24 13:42:31 +0530

    Update config.js

--- util/config.js
+++ util/config.js
@@ -11,8 +11,8 @@ onboot = require('./onboot');
 
 module.exports = {
   onboot: onboot,
-  auth: process.env.token,
-  channel: process.env.channel || '1055794364783272006',
+  auth: process.env.token, // bot token
+  channel: process.env.channel, // voice/stage channel id
   status: {
     afk: false,
     status: 'idle', // idle, dnd, online, invisible
@@ -23,22 +23,4 @@ module.exports = {
     }]
   },
   interval: 6 * 60 * 60 * 1000, // interval to switch to random stream in milliseconds
-  sources: [ // responsive streams! just put in your sources below and enjoy :)
-    {
-      topic: 'I Love Radio', // for stage channels (stage topic)
-      url: 'https://streams.ilovemusic.de/iloveradio1.mp3' // source
-    },
-    {
-      topic: 'I Love The Sun',
-      url: 'https://streams.ilovemusic.de/iloveradio15.mp3'
-    },
-    {
-      topic: 'I Love Music & Chill',
-      url: 'https://streams.ilovemusic.de/iloveradio10.mp3'
-    },
-    {
-      topic: 'I Love Greatest Hits',
-      url: 'https://streams.ilovemusic.de/iloveradio16.mp3'
-    }
-  ]
 }
